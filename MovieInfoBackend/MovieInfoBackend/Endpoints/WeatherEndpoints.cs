5c2237e48ab311642421b104b1417cbd518ad0d2
Edward Younskevicius <eyounskevicius@fullsail.com>
2025-10-07 16:19:16 -0700

    Adding ASP.Net Core and React files

---
+++ MovieInfoBackend/MovieInfoBackend/Endpoints/WeatherEndpoints.cs
@@ -0,0 +1,31 @@
+namespace MovieInfoBackend.Endpoints;
+
+public class WeatherEndpoints
+{
+    public static void Map(WebApplication app)
+    {
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        app.MapGet("/weatherforecast", () =>
+            {
+                var forecast = Enumerable.Range(1, 5).Select(index =>
+                        new WeatherForecast
+                        (
+                            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                            Random.Shared.Next(-20, 55),
+                            summaries[Random.Shared.Next(summaries.Length)]
+                        ))
+                    .ToArray();
+                return forecast;
+            })
+            .WithName("GetWeatherForecast");
+    }
+    
+    record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
