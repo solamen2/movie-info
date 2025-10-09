fbbc9353ddb5a1394e342fa646bcf2a34d1f3abf
Edward Younskevicius <eyounskevicius@fullsail.com>
2025-10-08 17:05:30 -0700

    -- Add proxy for backend to frontend (and debug handlers to "configure"  -- Add plugin-basic-ssl to support proxy in development  -- Move backend route to "/api"

--- MovieInfoBackend/MovieInfoBackend/Endpoints/WeatherEndpoints.cs
+++ MovieInfoBackend/MovieInfoBackend/Endpoints/WeatherEndpoints.cs
@@ -9,7 +9,7 @@ public class WeatherEndpoints
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        app.MapGet("/weatherforecast", () =>
+        app.MapGet("/api/weatherforecast", () =>
             {
                 var forecast = Enumerable.Range(1, 5).Select(index =>
                         new WeatherForecast
--- movie-info-frontend/vite.config.ts
+++ movie-info-frontend/vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react(), basicSsl()],
+    server: {
+        port: 5000,
+        strictPort: true,
+        proxy: {
+            "/api": {
+                target: "https://localhost:7184",
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/api/, "/api"),
+                configure: (proxy, _options) => {
+                    proxy.on('error', (err, _req, _res) => {
+                        console.log('proxy error', err);
+                    });
+                    proxy.on('proxyReq', (_proxyReq, req, _res) => {
+                        console.log('Sending Request to the Target:', req.method, req.url);
+                    });
+                    proxy.on('proxyRes', (proxyRes, req, _res) => {
+                        console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+                    });
+                },
+            },
+        },
+    },
+});
