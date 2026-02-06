d64f8c8aa50595b65c629df007ba9a179d2d6a37
Edward Younskevicius <eyounskevicius@fullsail.com>
2026-02-06 14:48:12 -0800

    -- Add initial Bruno config for ad-hoc testing of stage / prod -- Fix ProgramConfig to use correct value in checked-in version

--- MovieInfoBackend/MovieInfoBackend/ProgramConfig.cs
+++ MovieInfoBackend/MovieInfoBackend/ProgramConfig.cs
@@ -3,5 +3,5 @@ public class ProgramConfig
     public static readonly TimeSpan LoginCookieTimeout = TimeSpan.FromDays(1);
 
     // Development-only config
-    public static readonly LocalDbConnType DbConnType = LocalDbConnType.Local;
+    public static readonly LocalDbConnType DbConnType = LocalDbConnType.AzureDev;  // should always be set to AzureDev in checked-in version
 }
