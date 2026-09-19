2e5fa8733a1817b75781519d9a472e853071b021
Edward Younskevicius <eyounskevicius@fullsail.com>
2026-09-18 21:40:15 -0700

    Set login cookie time to 1 week

--- MovieInfoBackend/MovieInfoBackend/ProgramConfig.cs
+++ MovieInfoBackend/MovieInfoBackend/ProgramConfig.cs
@@ -3,7 +3,7 @@ using System.Diagnostics.CodeAnalysis;
 [ExcludeFromCodeCoverage]
 public class ProgramConfig
 {
-    public static readonly TimeSpan LoginCookieTimeout = TimeSpan.FromMinutes(30);
+    public static readonly TimeSpan LoginCookieTimeout = TimeSpan.FromDays(7);
 
     // Development-only config
     public static readonly LocalDbConnType DbConnType = LocalDbConnType.AzureDev;  // should always be set to AzureDev in checked-in version
